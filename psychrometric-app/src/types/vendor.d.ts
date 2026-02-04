ef3a1981036b4201ff7fee4d01ceebe482acfeab
kn-nakamura <154112427+kn-nakamura@users.noreply.github.com>
2026-02-04 23:48:18 +0900

    Add jsPDF SVG typings

--- psychrometric-app/src/types/vendor.d.ts
+++ psychrometric-app/src/types/vendor.d.ts
@@ -8,3 +8,17 @@ declare module 'canvas2svg' {
 }
 
 declare module 'svg2pdf.js';
+
+declare module 'jspdf' {
+  interface jsPDF {
+    svg(
+      element: SVGElement,
+      options?: {
+        x?: number;
+        y?: number;
+        width?: number;
+        height?: number;
+      }
+    ): Promise<void>;
+  }
+}
